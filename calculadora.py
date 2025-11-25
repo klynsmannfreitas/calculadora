658175250f186b2039aeffafa4796b80996ad4cf
klynsmann <klynsmannfreitas@gmail.com>
2025-11-25 11:26:56 -0400

    Colocar multiplicação

--- calculadora.py
+++ calculadora.py
@@ -3,7 +3,8 @@ def soma(n1: float, n2: float):
     return n1 + n2
 
 #TODO: Klynsmann cria a função de multplicação
-
+def multplicacao(n1: float, n2: float):
+    return n1 * n2
 
 
 #TODO: Roberto cria subtração
