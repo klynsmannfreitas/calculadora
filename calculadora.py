1d9ea96c33420448378470f76bb96e40caaee59c
klynsmann <klynsmannfreitas@gmail.com>
2025-11-25 11:17:37 -0400

    nova funçao

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+#funções aritimeticas 
+def soma(n1: float, n2: float):
+    return n1 + n2
+
+#TODO: Klynsmann cria a função de multplicação
+def multplicacao(n1: float, n2: float):
+    return n1 * n2
+
+
+#TODO: Roberto cria subtração
---
+++ main.py
@@ -0,0 +1,6 @@
+from calculadora import soma
+
+n1 = input("informe o priemiro número: ")
+n2 = input("informe o priemiro número: ")
+
+print(f"A soma de {n1} + {n2} é {soma(n1, n2)}")
