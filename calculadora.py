889e1d4adcc02bdc282974030b42f59e64c412cf
Roberto Sileno <robertosileno.1@gmail.com>
2025-11-26 10:54:29 -0400

    Função subtração inserida

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,11 @@
-#funções aritimeticas 
+#funções aritmeticas 
 def soma(n1: float, n2: float):
     return n1 + n2
 
-#TODO: Klynsmann cria a função de multplicação
+#TODO: Klynsmann cria a função de multiplicação
 
 
 
-#TODO: Roberto cria subtração
+#TODO: Roberto cria a função de subtração
 def subtracao(n1: float, n2: float):
     return n1 - n2
