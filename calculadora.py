82c0612ddf36946bd1950ade6804bc45497a7789
Roberto Sileno <robertosileno.1@gmail.com>
2025-11-27 08:13:33 -0400

    Função mult retirada

--- calculadora.py
+++ calculadora.py
@@ -6,15 +6,5 @@ def soma(n1: float, n2: float):
 
 
 
-#TODO: Roberto cria subtração 
-    
-#funções aritimeticas 
-def soma(n1: float, n2: float):
-    return n1 + n2
-
-#TODO: Klynsmann cria a função de multplicação
-def multplicacao(n1: float, n2: float):
-    return n1 * n2
-
 
 #TODO: Roberto cria subtração 
